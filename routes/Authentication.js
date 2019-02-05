68cd8d3a3f79fecbde8eead44c970af2f86a10fd
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2019-02-05 10:39:56 -0500

    Add router.post('register')
    Insert a new user in db

---
+++ routes/Authentication.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var AuthenticationController = require('../controllers/Authentication');
+var router = express.Router();
+
+router.post('register', AuthenticationController.register);
+
+module.exports = router;
